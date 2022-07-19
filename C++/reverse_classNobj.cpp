30c291ed3aab8e23b0495aa924415edf4ccb26e4
Nayak2904 <nayakshivajee@gmial.com>
2022-07-19 14:01:00 +0530

    files added

---
+++ C++/reverse_classNobj.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Reverse
+{
+public:
+} int main()
+{
+    int num;
+    cout << "Enter any number:" << endl;
+    cin >> num;
+
+    cout << "Reversed number:" << reverse(num) << endl;
+    return 0;
+}

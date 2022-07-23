9dd99af8e44bbd55576b3883e452731393fe3122
Nayak2904 <nayakshivajee@gmial.com>
2022-07-23 15:40:28 +0530

    files added

---
+++ C++/timepass.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What' your name?" << endl;
+    cin >> name;
+
+    cout << "What?" << endl;
+    cin >> name;
+
+    cout << "Fuck you " << name << endl;
+    return 0;
+}

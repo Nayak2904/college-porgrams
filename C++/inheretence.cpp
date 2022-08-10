734d2618f3cb9fad64b320235a5a41390924e788
Nayak2904 <nayakshivajee@gmial.com>
2022-08-10 14:51:04 +0530

    added

---
+++ C++/inheretence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class base
+{
+protected:
+    int age;
+    char name[20];
+};
+class derived : public base
+{
+protected:
+    int height;
+    int weight;
+
+public:
+    void get()
+    {
+        cout << "Enter name:" << endl;
+        cin >> name;
+        cout << "Enter age" << endl;
+        cin >> age;
+        cout << "Enter height and weight:" << endl;
+        cin >> height >> weight;
+    }
+    void show()
+    {
+        cout << "\nStudent's DATA:\n"
+             << endl;
+        cout << "Name:" << name << endl;
+
+        cout << "Age" << age << endl;
+
+        cout << "Height and Weight:" << height << weight << endl;
+    }
+};
+int main()
+{
+    derived ob;
+    ob.get();
+    ob.show();
+    return 0;
+}

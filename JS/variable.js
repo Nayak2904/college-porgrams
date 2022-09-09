5814a63388dfb865039e6e40f143c3ac4560f2d1
Nayak2904 <nayakshivajee@gmail.com>
2022-09-09 11:12:40 +0530

    update

---
+++ JS/variable.js
@@ -0,0 +1,9 @@
+let Name = 'Shivajee'
+let isStudent = true
+let emptyValue = null 
+let lastNam = undefined
+console.log(typeof emptyValue)
+console.log(typeof isStudent)
+console.log(typeof lastName)
+console.log(typeof Name)
+console.log(Name, isStudent, emptyValue, lastNam)

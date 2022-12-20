dd1d23824cbe7a725af1e422d7023715ee43bf94
Nayak2904 <nayakshivajee@gamil.com>
2022-12-20 21:57:44 +0530

    files added

--- JAVA/sum.java
+++ JAVA/sum.java
@@ -2,9 +2,10 @@ public class sum {
     public static void main(String[] args) {
         int n1 = 22;
         int n2 = 22;
+        int n3 = 22;
         int sum;
-        sum = n1+n2;
+        sum = n1+n2+n3;
 
-        System.out.println("The sum of thw number is:" +sum);
+        System.out.println("The sum of n1, n2 & n3 is:" +sum);
     }
 }
